aa9a54369370f86d35cf8688f528bf532dcede83
Aldi Andalan <andalanaldi@gmail.com>
2023-11-30 16:03:57 +0700

    chore: re-check db model for password to ensure auth has same length

--- backend-w22/user/models.py
+++ backend-w22/user/models.py
@@ -4,6 +4,7 @@ from db import db
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False) 
+    # Ensure this line above aligns with the password length constraint between the password validation constraint and the database column length
     role = db.Column(db.String(20), nullable=False, default='client')
     
