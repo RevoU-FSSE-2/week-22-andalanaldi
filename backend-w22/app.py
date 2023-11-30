631b8d205aec773b90162c6ee5a0d05cb90d95c4
Aldi Andalan <andalanaldi@gmail.com>
2023-11-30 16:38:09 +0700

    feat: add more detail content security policy to talisman

--- backend-w22/app.py
+++ backend-w22/app.py
@@ -10,7 +10,33 @@ from ipotodow.ipo_service import ipo_service
 
 app = Flask(__name__)
 CORS(app)
-Talisman(app)
+
+# Configuration to match helmet middleware settings in express js
+csp = {
+    'default-src': '\'self\'',
+    'frame-ancestors': '\'none\'',
+    'base-uri': '\'self\'',
+    'object-src': '\'none\'',
+    'img-src': '*',
+}
+
+talisman = Talisman(
+    app,
+    content_security_policy=csp,
+    frame_options='deny',
+    hide_powered_by=True,
+    force_https=True,  # Equivalent to HSTS in helmet
+    force_https_permanent=True,
+    referrer_policy='strict-origin-when-cross-origin',
+    session_cookie_secure=True,
+    session_cookie_http_only=True,
+    content_type_nosniff=True,
+    feature_policy={
+        'geolocation': '\'none\'',
+        'midi': '\'none\'',
+        'sync-xhr': '\'none\'',
+    }
+)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db.init_app(app)
