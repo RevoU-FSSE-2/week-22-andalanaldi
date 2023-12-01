9c97d282ace4e1e2f7b6d6ef6fe644e909227949
Aldi Andalan <andalanaldi@gmail.com>
2023-12-02 04:50:17 +0700

    fix: set talisman to default, set db init for db migration

--- backend-w22/app.py
+++ backend-w22/app.py
@@ -11,34 +11,35 @@ from ipotodow.apis import ipo_service
 
 app = Flask(__name__)
 CORS(app)
+Talisman(app)
 
-# Configuration to match helmet middleware settings in express js SELF = "'self'"
-csp = {
-    'default-src': '\'self\'',
-    'frame-ancestors': '\'none\'',
-    'base-uri': '\'self\'',
-    'object-src': '\'none\'',
-    'img-src': '*',
-}
+# # Configuration to match helmet middleware settings in express js SELF = "'self'"
+# csp = {
+#     'default-src': '\'self\'',
+#     'frame-ancestors': '\'none\'',
+#     'base-uri': '\'self\'',
+#     'object-src': '\'none\'',
+#     'img-src': '*',
+# }
 
-talisman = Talisman(
-    app,
-    content_security_policy=csp,
-    frame_options='deny',
-    content_security_policy_nonce_in=['script-src','style-src'],
-    hide_powered_by=True, #
-    force_https=True,  # Equivalent to HSTS in helmet
-    force_https_permanent=True,
-    referrer_policy='strict-origin-when-cross-origin',
-    session_cookie_secure=True,
-    session_cookie_http_only=True,
-    content_type_nosniff=True, #
-    feature_policy={
-        'geolocation': '\'none\'',
-        'midi': '\'none\'',
-        'sync-xhr': '\'none\'',
-    }
-)
+# talisman = Talisman(
+#     app,
+#     content_security_policy=csp,
+#     frame_options='deny',
+#     content_security_policy_nonce_in=['script-src','style-src'],
+#     hide_powered_by=True, #
+#     force_https=True,  # Equivalent to HSTS in helmet
+#     force_https_permanent=True,
+#     referrer_policy='strict-origin-when-cross-origin',
+#     session_cookie_secure=True,
+#     session_cookie_http_only=True,
+#     content_type_nosniff=True, #
+#     feature_policy={
+#         'geolocation': '\'none\'',
+#         'midi': '\'none\'',
+#         'sync-xhr': '\'none\'',
+#     }
+# )
 
 database_url = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
@@ -49,8 +50,8 @@ app.register_blueprint(auth_service, url_prefix='/auth')
 app.register_blueprint(ipo_service, url_prefix='/ipo')
 
 
-with app.app_context():
-    db_init()
+# with app.app_context():
+#     db_init()
 
 # pipenv install flask flask-cors V
 # pipenv install pymongo X
